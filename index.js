ccbae60d7735c6b7a2d8d6e8b75616eb9b82c4a4
TN Roebuck <tnroebuck@TNs-MacBook-Pro.local>
2019-12-17 16:31:19 -0700

    finished counterMaker

--- index.js
+++ index.js
@@ -277,12 +277,12 @@ function tallyUpDonations(runners) {
 */
 function counterMaker() {
   // BROKEN CODE STARTS
-  const count = 0;
-  function counter() {
-    ++count
+  let count = -1;
+  return function counter() {
+   return ++count;
   }
   // BROKEN CODE ENDS
-}
+};
 
 /**
  * ### Challenge `counterMakerWithLimit`
